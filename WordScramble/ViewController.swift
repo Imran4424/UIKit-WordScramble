2c25bc3b80d89a81429a719e099159e083dda6c4
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2022-08-28 11:12:13 +0600

    words

--- WordScramble/ViewController.swift
+++ WordScramble/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    var allWords = [String]()
+    var usedWords = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
